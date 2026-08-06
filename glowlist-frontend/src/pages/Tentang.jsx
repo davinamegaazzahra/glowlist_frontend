045ea24a169c21a0a5dfa5d68783cf3bf57d7361
davinamegaazzahra <davinamegaazzahra649@gmail.com>
2026-08-06 11:12:26 +0700

    update terbaru lagi

---
+++ glowlist-frontend/src/pages/Tentang.jsx
@@ -0,0 +1,18 @@
+export default function Tentang() {
+    return (
+        <div style={{ padding: "20px" }}>
+            <h2>Tentang Saya ✨</h2>
+
+            <p><strong>Nama :</strong> Davina Mega Azzahra Cahyaning p. </p>
+            <p><strong>Kelas:</strong> XII RPL 1 </p>
+
+            <h3>Saya Tidak Suka Skincare</h3>
+            <p>
+                Saya tidak suka skincare karna Mahal,
+                tidak tahu jenis-jenisnya dan saya
+                hanya menggunakan sabun cuci muka
+                dari Glow And Lovely.facial wash
+            </p>
+        </div>
+    )
+}
